9fda630f3ffd74686e8621f6ba68e41cb039ba19
Jose Reyna <dariuzcm@gmail.com>
2024-05-20 13:41:17 -0600

    init project

---
+++ config/database.ts
@@ -0,0 +1,37 @@
+import Env from '#start/env'
+import app from '@adonisjs/core/services/app'
+import { defineConfig } from '@adonisjs/lucid'
+
+const dbConfig = defineConfig({
+  connection: Env.get('DB_CONNECTION', 'mssql'),
+  connections: {
+    sqlite: {
+      client: 'better-sqlite3',
+      connection: {
+        filename: app.tmpPath('db.sqlite3'),
+      },
+      useNullAsDefault: true,
+      migrations: {
+        naturalSort: true,
+        paths: ['database/migrations'],
+      },
+    },
+    mssql: {
+      client: 'mssql',
+      connection: {
+        server: Env.get('DB_HOST', 'localhost'),
+        host: Env.get('DB_HOST', 'localhost'),
+        user: Env.get('DB_USER'),
+        password: Env.get('DB_PASSWORD'),
+        database: Env.get('DB_DATABASE'),
+        port: 1434,
+        options: {
+          encrypt: false,
+          trustServerCertificate: true,
+        },
+      },
+    },
+  },
+})
+
+export default dbConfig
